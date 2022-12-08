e299e92086706c3d646edc5ffa0351886f635f28
imStav <saprocontact@gmail.com>
2022-12-08 22:55:52 +0100

    Vuetify sandbox!
    Initializing project.
    A Vue 3 + Vuetify 3 sandbox project to discover Vuetify 3.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <HelloWorld msg="You did it!" />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-block: 2rem;
+}
+
+nav {
+  margin-inline: auto;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Card from '../components/Card.vue'
+</script>
+
+<template>
+  <main>
+    <Card />
+  </main>
+</template>
