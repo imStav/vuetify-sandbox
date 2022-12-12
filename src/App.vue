9b556bade50b59f2b42a689afdb2e35d62440af4
imStav <saprocontact@gmail.com>
2022-12-12 13:23:32 +0100

    New `ArchivesView` page
    This `vue` will receive 'archived' Cards.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/archives">Archives</RouterLink>
     </nav>
   </header>
 
---
+++ src/views/ArchivesView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Archives</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: grid;
+  max-width: 50%;
+  margin-inline: auto;
+  place-content: center;
+  place-items: center;
+}
+</style>
